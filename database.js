b14d10377c8b282717d3da145d817df520228678
VS11-coder <vasudevsarraf11@gmail.com>
2024-12-09 16:42:24 +0530

    completed database.js

--- database.js
+++ database.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 mongoose.connect("mongodb+srv://vasudevsarraf11:Va100sa1@cluster0.ooh1h.mongodb.net/User");
-const User = mongoose.model('Client', { name: String, email: String, password: String });
+const User = mongoose.model('Users', { name: String, email: String, password: String });
+
 app.post("/signup", async function(req,res) {
   const name = req.body.name;
   const username = req.body.username;
@@ -12,15 +13,20 @@ app.post("/signup", async function(req,res) {
   if ( existingUser ) {
     return res.status(400).send("Username already exists");
   }
-  const user = new User({
-    name : name,
-    email : username,
-    password : password
-  });
-  user.save();
-  res.join({
+  await User.create({ name, email:username, password});
+  // const user = new User({
+  //   name : name,
+  //   email : username,
+  //   password : password
+  // });
+  // user.save();
+  res.send({
     "msg" : "User created successfully"
   })
+})
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
 })
 
 
@@ -41,3 +47,5 @@ app.post("/signup", async function(req,res) {
 //   User,
 //   UpiTransaction,
 // };
+
+
